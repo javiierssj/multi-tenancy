6484d8065c854efcbf8728e4a86b9c5582e22689
Joan Toro Ortiz <joantoro@me.com>
2024-01-03 21:57:44 -0300

    fix correct!

--- src/middleware/tenantMiddleware.js
+++ src/middleware/tenantMiddleware.js
@@ -1,34 +1,21 @@
-const dbConnectionManager = require('../utils/dbConnectionManager');
+const Tenant = require('../models/Tenant'); // Modelo para acceder a la lista de tenants
 
-/**
- * Middleware para manejar el multi-tenancy.
- * Identifica al tenant basado en la solicitud y establece la conexión a la base de datos correspondiente.
- */
 const tenantMiddleware = async (req, res, next) => {
   try {
-    // Identificar el tenant. Esto puede variar según tu enfoque, por ejemplo:
-    // 1. Subdominio: tenantId = req.subdomains[0] =>     const subdomain = req.hostname.split('.')[0];
-    // 2. Encabezado HTTP: tenantId = req.headers['X-Tenant-ID'] => const tenantId = req.headers['x-tenant-id'];
-    // 3. Otro método basado en tu lógica de aplicación.
     const subdomain = req.hostname.split('.')[0];
+    const tenant = await Tenant.findOne({ subdomain });
 
-    if (!subdomain) {
-      return res.status(400).send({ error: 'Subdomain not provided' });
+    if (!tenant) {
+      return res.status(403).send({ error: 'Tenant not allowed' });
     }
 
-    // Aquí puedes tener una lógica para manejar "www" o subdominios no válidos
-    // Por ejemplo, ignorar 'www' o enviar error si el subdominio no está en la lista de tenants permitidos
-
-    // Obtener o crear una conexión a la base de datos para el tenant identificado por subdominio
     const dbConnection = await dbConnectionManager.getConnectionForTenant(subdomain);
-
-    // Adjuntar la conexión a la base de datos y el subdominio a la solicitud
     req.dbConnection = dbConnection;
     req.tenantSubdomain = subdomain;
 
     next();
   } catch (error) {
-    return res.status(500).send({ error: 'Failed to establish tenant database connection' });
+    return res.status(500).send({ error: 'Error verifying tenant' });
   }
 };
 
