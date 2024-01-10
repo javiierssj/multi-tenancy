4ee399f3dc386496ff30c1de880a1733b789e0ae
Diego Diaz <81176354+DDiazGithub@users.noreply.github.com>
2024-01-10 20:07:38 -0300

    fix token validate

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -14,7 +14,7 @@ const auth = async (req, res, next) => {
       throw new Error('Invalid token');
     }
 
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+    const user = await User.findOne({ _id: decoded._id});
     if (!user) {
       throw new Error('User not found');
     }
