8d17cb5024945f1b9671ace15737d5cfbfec45ce
Joan Toro <joantoro@me.com>
2024-01-11 20:52:01 -0300

    fix router

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -7,7 +7,7 @@ const authMiddleware = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/', authMiddleware, productController.createProduct);
-router.get('/', authMiddleware, productController.getAllProduct);
+router.get('/', authMiddleware, productController.getAllProducts);
 router.get('/:taskId', authMiddleware, productController.getProductById);
 router.patch('/:taskId', authMiddleware, productController.updateProduct);
 router.delete('/:taskId', authMiddleware, productController.deleteProduct);
